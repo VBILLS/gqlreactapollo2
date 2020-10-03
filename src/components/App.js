3420ec7dda731d49199fa81087f419a79927a9c6
Veronica Bills <vbills123@gmail.com>
2020-10-03 16:45:22 -0400

    server is a subgit directory... trying to figure it out

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LinkList from './LinkList'
+
+import '../styles/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <LinkList />
+    );
+  }
+}
+
+export default App;
